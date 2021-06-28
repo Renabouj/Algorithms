1828aae934ab2a84db60f6ec9bb0e76c178b3737
Renabouj <rgf@cin.ufpe.br>
2021-06-28 17:17:31 -0300

    Doctesting

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -8,9 +8,14 @@ def maxSubArr(nums: List[int]) -> int:
     for k in range(1, len(nums)):
         sub_arr[k] = max((sub_arr[k - 1] + nums[k]), nums[k])
         if sub_arr[k] > max_numb: max_numb = sub_arr[k]
-        print(sub_arr)
-        print(max_numb)
     return max_numb
 
-print(maxSubArr([-4, 3, 2, -1, 10, -9, 2]))
-#result should be: 14 
+def main():
+    """
+    >>> maxSubArr([-4, 3, 2, -1, 10, -9, 2])
+    14
+    """
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
